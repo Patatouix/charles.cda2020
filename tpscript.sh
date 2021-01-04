bee6136177e41f8aea411a20dbbae84427ae5508
Charles Anguenot <anguenot@gmail.com>
2021-01-04 14:18:23 +0000

    Compression du dump sql

--- tpscript.sh
+++ tpscript.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #tar -zcvf wparchive.tar.gz /var/www/html/WP
 tar -jcvf wparchive.tar.bz2 /var/www/html/WP
-MYSQL_PWD="Magiciendoz2704!" mysqldump -u wordpress > wordpress.sql
+MYSQL_PWD="Magiciendoz2704!" mysqldump -u wordpress | gzip > wordpress.sql.gz
