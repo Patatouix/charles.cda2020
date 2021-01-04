40aed1593fb169f090f4d6bc7a1ea8363be6e17d
Charles Anguenot <anguenot@gmail.com>
2021-01-04 14:29:12 +0000

    Modification de l'archivage bdd

--- tpscript.sh
+++ tpscript.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-#tar -zcvf wparchive.tar.gz /var/www/html/WP
+tar -zcvf wparchive.tar.gz /var/www/html/WP
 tar -jcvf wparchive.tar.bz2 /var/www/html/WP
-MYSQL_PWD="Magiciendoz2704!" mysqldump -u wordpress | gzip > wordpress.sql.gz
+MYSQL_PWD="Magiciendoz2704!" mysqldump -u userwp wordpress | gzip > wordpress.sql.gz
+MYSQL_PWD="Magiciendoz2704!" mysqldump -u userwp wordpress | bzip2 > wordpress.sql.bz2
